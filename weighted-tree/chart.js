74116ac62f9fa8372c4dca8915b726be0a1905ed
Nandor Devai <mail@nandordevai.net>
2019-09-28 22:53:03 +0200

    basic tree layout

---
+++ weighted-tree/chart.js
@@ -0,0 +1,29 @@
+import { data } from './data.js';
+
+const root = d3.hierarchy(data);
+const treeLayout = d3.tree();
+treeLayout.size([800, 400]);
+treeLayout(root);
+
+// Nodes
+d3.select('svg g.nodes')
+  .selectAll('circle.node')
+  .data(root.descendants())
+  .enter()
+  .append('circle')
+  .classed('node', true)
+  .attr('cx', function(d) {return d.x;})
+  .attr('cy', function(d) {return d.y;})
+  .attr('r', 4);
+
+// Links
+d3.select('svg g.links')
+  .selectAll('line.link')
+  .data(root.links())
+  .enter()
+  .append('line')
+  .classed('link', true)
+  .attr('x1', function(d) {return d.source.x;})
+  .attr('y1', function(d) {return d.source.y;})
+  .attr('x2', function(d) {return d.target.x;})
+  .attr('y2', function(d) {return d.target.y;});
