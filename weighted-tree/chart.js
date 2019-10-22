94ce5176f172cf96c3430a9d15cc4ab9bcbbc691
Nandor Devai <mail@nandordevai.net>
2019-10-22 17:44:58 +0200

    add sizing & fix links

--- weighted-tree/chart.js
+++ weighted-tree/chart.js
@@ -2,28 +2,42 @@ import { data } from './data.js';
 
 const root = d3.hierarchy(data);
 const treeLayout = d3.tree();
-treeLayout.size([800, 400]);
+treeLayout.size([600, 600]);
 treeLayout(root);
+
+function nodeSize(d) {
+  return Math.max(1, Math.round(d.data.rank / 5));
+}
 
 // Nodes
 d3.select('svg g.nodes')
   .selectAll('circle.node')
   .data(root.descendants())
-  .enter()
-  .append('circle')
-  .classed('node', true)
-  .attr('cx', function(d) {return d.x;})
-  .attr('cy', function(d) {return d.y;})
-  .attr('r', 4);
+  .join(
+    (enter) => enter
+      .append('circle')
+      .classed('node', true)
+      .attr('cx', d => d.y)
+      .attr('cy', d => d.x)
+      .attr('r', d => `${nodeSize(d) / 2}px`)
+  );
 
 // Links
+const link = d3.linkHorizontal()
+  .x(d => d.y)
+  .y(d => d.x);
+
 d3.select('svg g.links')
-  .selectAll('line.link')
+  .selectAll('path.link')
   .data(root.links())
-  .enter()
-  .append('line')
-  .classed('link', true)
-  .attr('x1', function(d) {return d.source.x;})
-  .attr('y1', function(d) {return d.source.y;})
-  .attr('x2', function(d) {return d.target.x;})
-  .attr('y2', function(d) {return d.target.y;});
+  .join(
+    (enter) => enter
+      .append('path')
+      .classed('link', true)
+      .attr('x1', d => d.source.y)
+      .attr('y1', d => d.source.x)
+      .attr('x2', d => d.target.y)
+      .attr('y2', d => d.target.x)
+      .attr('d', link)
+      .attr('stroke-width', d => `${nodeSize(d.target)}px`)
+  );
